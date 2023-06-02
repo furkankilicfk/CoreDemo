0c528f4a4892051c1d30be7783e31ee2428b48ae
Furkan Kılıç <furkankilicfk@outlook.com>
2023-06-02 21:23:59 +0300

    API-Postman-Swagger

---
+++ BlogApiDemo/Controllers/DefaultController.cs
@@ -0,0 +1,26 @@
+﻿using BlogApiDemo.DataAccessLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace BlogApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]  //bunu eklemezsek api ne yapacağını bilmediğinden(ekleme silme güncelleme) çalıştırmaz--attribute şart
+        public IActionResult EmployeeList()
+        {
+            using var c = new Context();
+            var values = c.Employees.ToList();
+            return Ok(values); //apide başarılı olan isteklerin sonucunda dönecek status kodu
+        }
+
+        [HttpPost]
+        public IActionResult EmployeeAdd()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ BlogApiDemo/DataAccessLayer/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApiDemo.DataAccessLayer
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-TJC13CJ;database=CoreBlogApiDb; integrated security=true;");
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
